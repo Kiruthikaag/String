cdde14c4374952df8f0b04bf3e61a0a484ff9329
Kiruthikaag <agkiruthikaait13@gmail.com>
2016-07-16 17:52:50 +0530

    Add files via upload

---
+++ Mu.java
@@ -0,0 +1,12 @@
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i,a=1,s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextInt();
+        for(i=0;i<=10;i++){
+            s=i*a;
+            
+        }
+        System.out.println("the table is"+i+"*"+a+"="+s);
+    }
