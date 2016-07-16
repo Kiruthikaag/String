3a7158e76ffba3c2eb50b3b0bf0f959ca16dafab
Kiruthikaag <agkiruthikaait13@gmail.com>
2016-07-16 10:49:48 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("even");
+            
+        }else{
+            System.out.println("odd");
+        }
+    }
