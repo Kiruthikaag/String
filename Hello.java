2ba920499a20e14f1b4ca6d94b69632294ea3851
Kiruthikaag <agkiruthikaait13@gmail.com>
2016-07-16 14:49:36 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+        // TODO code application logic here
+        String s1;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+          for( int i=0;i<5;i++)
+      {
+            System.out.println("Hello World");
+        }
+    }
