6a9d66c3409ca138bc661d8862bef45fb92b0e3b
Kiruthikaag <agkiruthikaait13@gmail.com>
2016-07-16 13:54:58 +0530

    Add files via upload

---
+++ vow.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+     int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+      
+        if(n>=0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+       
+        }
