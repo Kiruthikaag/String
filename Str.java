5da330805340d0f859bc38335a1de2440d4ba855
Kiruthikaag <agkiruthikaait13@gmail.com>
2016-07-16 17:05:13 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        char a;
+        String s1;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        if(s1.equalsIgnoreCase("sunday")){
+            System.out.println("false");
+        }else{
+            System.out.println("true");
+        }
+    }
