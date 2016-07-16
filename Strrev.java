64e2c6a318b08a5fdf98f6fc6e0f5b0e6c03b74c
Kiruthikaag <agkiruthikaait13@gmail.com>
2016-07-16 17:34:13 +0530

    Add files via upload

---
+++ Strrev.java
@@ -0,0 +1,11 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+           String s,s1,r1 = null,r;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+       for (int j = s1.length() - 1; j >= 0; j--) {
+           System.out.println(s1);
+       }
+        r = r1 + s1.charAt(i); 
+    
+        System.out.println(r);
